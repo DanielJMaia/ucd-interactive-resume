32a19c169c1a7af74c9c8cb7890f23e51ae7aaa5
DanielJMaia <ubuntu@ip-172-31-35-15.ec2.internal>
2019-10-16 15:40:24 +0000

    updated the info to fit to Daniel Johnson Maia

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -9,9 +9,11 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            alert("Email Sent Successfully!")
         },
         function(error) {
             console.log("FAILED", error);
+            alert("Oops, seems like there was a problem. Try again later.")
         }
     );
     return false;  // To block from loading a new page
